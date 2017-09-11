3d585a826894570f34239308135cd83dd46137b4
chengxw <chengxw@servyou.com.cn>
2017-09-11 16:59:54 +0800

    压制了一些警告

--- src/bookbook/BuyServlet.java
+++ src/bookbook/BuyServlet.java
@@ -29,6 +29,7 @@ public class BuyServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());;
--- src/ccc/serverdemo14.java
+++ src/ccc/serverdemo14.java
@@ -3,7 +3,6 @@ package ccc;
 import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.Enumeration;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -12,8 +11,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
 
 /**
  * Servlet implementation class serverdemo14
